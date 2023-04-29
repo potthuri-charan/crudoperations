67611abbeb95223f9350a098381397cef1d5fecc
potthuri-charan <124122508+potthuri-charan@users.noreply.github.com>
2023-04-29 16:19:22 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
